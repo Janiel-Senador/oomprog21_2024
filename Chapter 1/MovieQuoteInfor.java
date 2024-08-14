c411280178aa719942475b4099b3438640da4313
Janiel Senador <154493279+Coosdad@users.noreply.github.com>
2024-08-14 19:22:41 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,8 @@
+public class MovieQuoteInfor {
+   public static void main(String args[]) {
+      System.out.println("She loved me at my worst. You had me at my best. At binalewala mo lang lahat yun.");
+      System.out.println("One More Chance");
+      System.out.println("2007");
+       System.out.println("starring John Lloyd Cruz");
+   }
+}
