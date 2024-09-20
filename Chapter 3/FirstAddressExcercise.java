ff711afbc220505403c16b0fe9cad1a506f49b77
Janiel Senador <154493279+Janiel-Senador@users.noreply.github.com>
2024-09-20 21:31:04 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExcercise {
+    public static void displayAddress() {
+        System.out.println("NTT DOCOMO, INC.");
+        System.out.println("11-1, Nagata-cho 2-chome");
+        System.out.println("Chiyoda-ku, Tokyo 100-6150 Japan");
+        System.out.println("Company: DOCOMO");
+    }
+    public static void main(String[] args) {
+        System.out.println();
+        displayAddress();
+    }
+}
