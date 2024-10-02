21ac3f940fb8a129c6bd584e266c683bb4b2bc0d
Janiel Senador <154493279+Janiel-Senador@users.noreply.github.com>
2024-10-02 23:14:15 +0800

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,13 @@
+public class MathTest {
+   public static void main(String[] args) {
+      System.out.println("SQRT "+Math.sqrt(37));
+      System.out.println("SIN "+Math.sin(300));
+      System.out.println("COS "+Math.cos(300));
+      System.out.println("FLOOR "+Math.floor(22.8));
+      System.out.println("CEIL "+Math.ceil(22.8));
+      System.out.println("ROUND "+Math.round(22.8));
+      System.out.println("MAX "+Math.max('D', 71));
+      System.out.println("MIN "+Math.min('D', 71));
+      System.out.println("RANDOM "+Math.random()*20);
+   }
+}
